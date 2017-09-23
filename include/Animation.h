08da70b7ee0c9885b5bf4f0ae4b66078fbc5f158
Arghadeep <bapanghosh.ghosh1@gmail.com>
2017-09-23 23:47:55 +0530

    a
    a

---
+++ include/Animation.h
@@ -0,0 +1,23 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+#include <SFML/Graphics.hpp>
+
+class Animation
+{
+    public:
+        Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime);
+        virtual ~Animation();
+        void update(int row, float deltaTime);
+
+        sf::IntRect uvRect;
+
+
+    private:
+        sf::Vector2u imageCount;
+        sf::Vector2u currentImage;
+
+        float switchTime;
+        float totalTime;
+};
+
+#endif // ANIMATION_H
